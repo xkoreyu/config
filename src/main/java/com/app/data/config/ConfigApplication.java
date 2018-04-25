a702f1140d5357a56395a5d3d586e6b2697c9e73
yu <xkorey@dingtalk.com>
2018-04-25 14:29:32 +0800

    add project config
    config project file

--- src/main/java/com/app/data/config/ConfigApplication.java
+++ src/main/java/com/app/data/config/ConfigApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @EnableConfigServer
 @EnableAutoConfiguration
+@Configuration
 @SpringBootApplication public class ConfigApplication {
 
     public static void main(String[] args) {
